a82f04510b91e39419890e186b875b5c392abb41
Nathan Campbell <nathancampbell@outlook.com>
2017-10-13 00:23:43 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,12 +6,9 @@ class RepositoriesController < ApplicationController
 
   def github_search
     begin
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
         req.params['client_id'] = 'Iv1.35cef91193676961'
         req.params['client_secret'] = '2c8e6b313b4dd6593a594e5c2ffa276b5e6492c8'
-        req.params['v'] = 'v3'
-        req.params['near'] = params[:zipcode]
-        req.params['query'] = 'coffee shop'
       end
       body = JSON.parse(@resp.body)
       if @resp.success?
