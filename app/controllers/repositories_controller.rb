69d465f0e08d2041d1b5422d1cd413c6e86fdbe6
Nathan Campbell <nathancampbell@outlook.com>
2017-10-13 00:25:15 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,10 +9,11 @@ class RepositoriesController < ApplicationController
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
         req.params['client_id'] = 'Iv1.35cef91193676961'
         req.params['client_secret'] = '2c8e6b313b4dd6593a594e5c2ffa276b5e6492c8'
+        req.params['q'] = params[:query]
       end
       body = JSON.parse(@resp.body)
       if @resp.success?
-        @venues = body["response"]["venues"]
+        @results = body["response"]["venues"]
       else
         @error = body["meta"]["errorDetail"]
       end
